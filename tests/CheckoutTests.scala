1fea0406887afe2079e10accfe71590ec346c03f
Jayden DeVile-Phoenix <jaydendevilephoenix@gmail.com>
2018-02-16 17:23:28 +0000

    added unit tests for step 2: simple offers

--- tests/CheckoutTests.scala
+++ tests/CheckoutTests.scala
@@ -23,4 +23,23 @@ class CheckoutTests {
     assertEquals(0, total, 0)
   }
 
+  // step 2: Simple offers
+  @Test def applyBogofDeal_whenCorrectDeductionCalculated_valueDoesMatchWithinDelta = {
+    val cart = List("Apple", "Apple", "Orange", "Apple")
+    val total = checkout.calculateTotal(cart) - checkout.applyBogofDeal("Apple", cart)
+    assertEquals(1.45f, total, 0.001f)
+  }
+
+  @Test def applyBogofDeal_whenListEmpty_valueDoesMatch = {
+    val cart = List()
+    val total = checkout.calculateTotal(cart) - checkout.applyBogofDeal("Apple", cart)
+    assertEquals(0, total, 0)
+  }
+
+  @Test def applyTheeForTwoDeal_whenCorrectDeductionCalculated_valueDoesMatchWithinDelta = {
+    val cart = List("Orange", "Orange", "Orange", "Apple")
+    val total = checkout.calculateTotal(cart) - checkout.applyThreeForTwoDeal("Orange", cart)
+    assertEquals(1.10f, total, 0.001f)
+  }
+
 }
