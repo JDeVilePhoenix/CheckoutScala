0311d42ed96f1c95c28e330e7dd4cb0b4b245533
Jayden DeVile-Phoenix <jaydendevilephoenix@gmail.com>
2018-02-16 17:14:20 +0000

    wrote a more scalable calculate total function and tested

--- src/Checkout.scala
+++ src/Checkout.scala
@@ -1,13 +1,24 @@
 class Checkout {
   private val products = Map("Apple" -> 0.60f, "Orange" -> 0.25f)
 
+  // step 1: Shopping cart
+  // a more scalable solution, can accept any number of mapped products
+  // takes a list of string product names and compares each element with the products map
+  // totaling the associated price of the mapped product if an empty list is passed or an incorrect
+  // product name the function returns 0
   def calculateTotal(cart:List[String]): Float = {
-    if (cart.nonEmpty) {
-      val total = (cart.count(_ == "Apple") * products("Apple")) +
-        (cart.count(_ == "Orange") * products("Orange"))
+    var total = 0f
+    if (cart.nonEmpty) try {
+      for (elem <- cart) {
+        total += products(elem)
+      }
       return total
-    } else
-      return 0f
+    } catch {
+      case e: NoSuchElementException => println("Unmapped product or spelling error: " + e)
+        return 0f
+    }
+    else
+      return total
   }
 
 }
